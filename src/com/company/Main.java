b4ab0d789200819c0cba24f864db53a9d372a7d7
Natasha S Love <debora@inbox.ru>
2019-09-13 10:13:30 -0500

    tecting loops and conditions
    getting the code working lilbylil

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,70 +5,84 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
 
 
-        int beginning=0;
-        int ending=0;
+        int beginning = 0;
+        int ending = 0;
         //String S="";
-        String subs="";
-        Scanner input=new Scanner(System.in);
+        String subs = "";
+        Scanner input = new Scanner(System.in);
         System.out.println("Enter your password.");
 
-        String S=input.nextLine();
-       // boolean uppercase=false;
-       // uppercase(S);
+        String S = input.nextLine();
+        // boolean uppercase=false;
+        // uppercase(S);
 
-        if (!uppercase(S))
-        {System.out.println("Invalid Password. Please try again.");}
+        if (!uppercase(S)) {
+            System.out.println("Invalid Password. Please try again.");
+        }
 
         //if string contains Uppercase (meets one condition) - look for a number in the string:
-        else if (uppercase(S))
-        {
-            for (int i=0; i<S.length();i++) {
-
+        else if (uppercase(S)) {
+            for (int i = 0; i < S.length(); i++) {
                 boolean number;
-                try{
+                //checking if chars are numbers
+                try {
                     Integer.parseInt(String.valueOf(S.charAt(i)));
-                    number=true;
-                } catch (NumberFormatException e){
-                    number=false;
-                    // break;//because found the first number
-                }
-                if (!number) {
+                    number = true;
+                    System.out.println("There're numbers only.Test1");
+                    //index of number in the string password
+                    // final int indexNum=i;
+                } catch (NumberFormatException e) {
+                    number = false;
+                    System.out.println("There's NON number char.Test1");
                     subs = S.substring(i);
-                    beginning=i;
-                    for (int j = 0; j < subs.length(); j++) {
+                    beginning = i;
+                    System.out.println("There's NON number char at index " + beginning+ ".Test2. "+ beginning+ " is equal to "+i);
 
-                        try{
-                            Integer.parseInt(String.valueOf(subs.charAt(j)));
-                            number=true;
-                        } catch (NumberFormatException e){
-                            number=false;
-                        }
-                        if (number){
-
-                            ending=j-1;
-                            break;
-                        }
-                        //too many for loops (inside) - need to separate
-
-                            for (int a=beginning;a<ending;a++) {
-                                if (Character.isUpperCase(subs.charAt(a))) {
-                                    subs=S.substring(beginning, ending);
-
-                                    System.out.println("Password is: " + subs);
-                                    break;
-
-                                }
-                                else    {
-                                    System.out.println("Something is wrong in the above code...");
-                                    break;
-                                }
-                            }
-
-                    }
+                    break;//because found the first NON number char
                 }
+                /*
+                if (!number) {
+                    //the first non num - char - beginning of the substring
+                    subs = S.substring(i);
+                    beginning = i;
+                    System.out.println("There's NON number char at index " + beginning+ ".Test2. "+ beginning+ " is equal to "+i);
+                }
+
+                 */
+            }
+
+//checking substring for more numbers char
+            for (int j = 0; j < subs.length(); j++) {
+                boolean number;
+                try {
+                    Integer.parseInt(String.valueOf(subs.charAt(j)));
+                    number = true;
+                } catch (NumberFormatException e) {
+                    number = false;
+                }
+                if (number) {
+                    ending = j - 1;
+                    break;
+                }
+            }
+            //too many for loops (inside) - need to separate
+            for (int a = beginning; a < ending; a++) {
+                if (Character.isUpperCase(subs.charAt(a))) {
+                    subs = S.substring(beginning, ending);
+                    System.out.println("Password is: " + subs);
+                    break;
+                } else {
+                    System.out.println("Something is wrong in the above code...");
+                    break;
+                }
+            }
+        }
+    }
+
+
                 /*
                 if (!number){
                     beginning=i;
@@ -78,10 +92,9 @@ public class Main {
 
                  */
 
-            }
 
-        }
-    }
+
+
 
     //check if there is uppercase- method
    public static boolean uppercase (String St){
